63811e34369591df291efb8bb3805a6b6eac5dd5
Chris Sargent <Chris.Sargent@blackswan.com>
2017-01-26 12:34:51 +0000

    Consolidated light class

--- src/main/java/org/sarge/jove/light/Light.java
+++ src/main/java/org/sarge/jove/light/Light.java
@@ -1,31 +1,106 @@
 package org.sarge.jove.light;
 
+import java.util.Optional;
+
 import org.sarge.jove.common.Colour;
 import org.sarge.jove.geometry.Point;
 import org.sarge.jove.geometry.Vector;
+import org.sarge.lib.util.Check;
+import org.sarge.lib.util.ToString;
 
 /**
  * Light.
  * @author Sarge
  */
-public interface Light {
+public final class Light {
 	/**
-	 * @return Type identifier (used to differentiate the different implementations in lighting shaders)
+	 * Creates am ambient light-source.
+	 * @param col Colour
+	 * @return Ambient light-source
 	 */
-	int getType();
+	public static Light ambient(Colour col) {
+		return new Light(col, null, null);
+	}
 
 	/**
-	 * @return Light colour
+	 * Creates a directional light-source, e.g. sunlight
+	 * @param col Colour
+	 * @param dir Direction
+	 * @return Directional light-source
 	 */
-	Colour getColour();
+	public static Light directional(Colour col, Vector dir) {
+		Check.notNull(col);
+		return new Light(col, dir, null);
+	}
 
 	/**
-	 * @return Light direction
+	 * Creates a point-source light, e.g. a bulb
+	 * @param col Colour
+	 * @param pos Position
+	 * @return Point-source light
 	 */
-	Vector getDirection();
+	public static Light pointSource(Colour col, Point pos) {
+		Check.notNull(col);
+		return new Light(col, null, pos);
+	}
 
 	/**
-	 * @return Light position
+	 * Creates a spot-light.
+	 * @param col Colour
+	 * @param pos Position
+	 * @param dir Direction
+	 * @return Spot-light
 	 */
-	Point getPosition();
+	public static Light spotLight(Colour col, Point pos, Vector dir) {
+		Check.notNull(col);
+		Check.notNull(col);
+		return new Light(col, dir, pos);
+		// TODO
+		//		cutoff
+		//		exponent
+		//		attenuation: constant, linear, quadratic
+	}
+	
+	private final Colour col;
+	private final Optional<Vector> dir;
+	private final Optional<Point> pos;
+
+	/**
+	 * Constructor.
+	 * @param col Colour
+	 * @param dir Direction
+	 * @param pos Position
+	 */
+	private Light(Colour col, Vector dir, Point pos) {
+		Check.notNull(col);
+		this.col = col;
+		this.dir = Optional.of(dir);
+		this.pos = Optional.of(pos);
+	}
+	
+	/**
+	 * @return Colour of this light
+	 */
+	public Colour getColour() {
+		return col;
+	}
+	
+	/**
+	 * @return Position of this light
+	 */
+	public Optional<Point> getPosition() {
+		return pos;
+	}
+
+	/**
+	 * @return Direction of this light
+	 */
+	public Optional<Vector> getDirection() {
+		return dir;
+	}
+	
+	@Override
+	public String toString() {
+		return ToString.toString(this);
+	}
 }
