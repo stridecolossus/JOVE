db4e79946de85a92ca360d0584ce8e39ea542826
stridecolossus <chris.sarge@gmail.com>
2021-11-08 10:23:53 +0000

    Added ZIP data source

---
+++ src/main/java/org/sarge/jove/util/ZipDataSource.java
@@ -0,0 +1,36 @@
+package org.sarge.jove.util;
+
+import static org.sarge.lib.util.Check.notNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * A <i>zip data source</i> provides read access to a ZIP archive.
+ * @author Sarge
+ */
+public class ZipDataSource implements DataSource {
+	private final ZipFile file;
+
+	/**
+	 * Constructor.
+	 * @param file ZIP archive
+	 */
+	public ZipDataSource(ZipFile file) {
+		this.file = notNull(file);
+	}
+
+	@Override
+	public InputStream input(String name) throws IOException {
+		final ZipEntry entry = file.getEntry(name);
+		return file.getInputStream(entry);
+	}
+
+	@Override
+	public OutputStream output(String name) throws IOException {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/test/java/org/sarge/jove/util/ZipDataSourceTest.java
@@ -0,0 +1,43 @@
+package org.sarge.jove.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ZipDataSourceTest {
+	private ZipDataSource src;
+	private ZipFile file;
+
+	@BeforeEach
+	void before() {
+		file = mock(ZipFile.class);
+		src = new ZipDataSource(file);
+	}
+
+	@Test
+	void input() throws IOException {
+		// Create entry
+		final String name = "name";
+		final ZipEntry entry = mock(ZipEntry.class);
+		when(file.getEntry(name)).thenReturn(entry);
+
+		// Check input stream
+		final InputStream in = mock(InputStream.class);
+		when(file.getInputStream(entry)).thenReturn(in);
+		assertEquals(in, src.input(name));
+	}
+
+	@Test
+	void output() {
+		assertThrows(UnsupportedOperationException.class, () -> src.output("whatever"));
+	}
+}
