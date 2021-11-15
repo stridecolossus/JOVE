37e781b1b71a57105d7d6dc274c435e47f958993
stridecolossus <chris.sarge@gmail.com>
2021-11-15 14:22:28 +0000

    Updated doc

--- src/main/java/org/sarge/jove/platform/vulkan/pipeline/PipelineLibrary.java
+++ src/main/java/org/sarge/jove/platform/vulkan/pipeline/PipelineLibrary.java
@@ -4,4 +4,5 @@ package org.sarge.jove.platform.vulkan.pipeline;
  * Vulkan pipeline API.
  */
 public interface PipelineLibrary extends Pipeline.Library, Shader.Library {
+	// Aggregate interface
 }
