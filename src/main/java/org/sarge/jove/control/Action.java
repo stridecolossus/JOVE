d1ac870c79df3b17f641bfd7d796aa2580d7088d
stridecolossus <chris.sarge@gmail.com>
2022-08-20 10:02:11 +0100

    Bindings cut #2

--- src/main/java/org/sarge/jove/control/Action.java
+++ src/main/java/org/sarge/jove/control/Action.java
@@ -3,39 +3,124 @@ package org.sarge.jove.control;
 import java.util.*;
 import java.util.function.Consumer;
 
-import org.sarge.jove.control.Event.Source;
-
+/**
+ * An <i>action</i> defines
+ * TODO
+ * @param <E> Event type
+ * @author Sarge
+ */
 public interface Action<E extends Event> {
-
+	/**
+	 *
+	 * @return
+	 */
 	String name();
 
+	/**
+	 *
+	 * @param event
+	 */
 	void execute(E event);
 
+	/**
+	 * An <i>action bindings</i> maps events to actions.
+	 */
 	class Bindings implements Consumer<Event> {
-		private final Map<Object, Runnable> handlers = new HashMap<>();
-		private final Map<Action<?>, Set<Runnable>> bindings = new HashMap<>();
+		private final Map<Event, Runnable> handlers = new HashMap<>();
+		private final Map<Action<?>, Set<Event>> bindings = new HashMap<>();
 
-		public <E extends Event> void bind(Source<E> source, Action<E> action) {
-
+		/**
+		 * @return Actions that can be bound
+		 */
+		public Set<Action<?>> actions() {
+			return Set.copyOf(bindings.keySet());
 		}
 
-		public void bind(Axis axis, Action<Axis> action) {
-			final Runnable handler = () -> action.execute(axis);
-			handlers.put(axis.type(), handler);
+		/**
+		 * Registers an action.
+		 * @param action Action
+		 */
+		public void add(Action<?> action) {
+			bindings.computeIfAbsent(action, ignored -> new HashSet<>());
 		}
 
-		public <T> void bind(Button<T> button, Action<Button<T>> action) {
-			final Runnable handler = () -> action.execute(button);
-			handlers.put(button.type(), handler);
+		/**
+		 * Maps the given action to its bindings.
+		 */
+		private Set<Event> map(Action<?> action) {
+			final Set<Event> events = bindings.get(action);
+			if(events == null) throw new IllegalStateException("Unknown action: " + action);
+			return events;
 		}
 
-		public void remove(Action<?> action) {
+		/**
+		 * Retrieves the bindings for the given action.
+		 * @param action Action
+		 * @return Bound events
+		 * @throws IllegalStateException if the action has not been registered
+		 */
+		public Set<Event> bindings(Action<?> action) {
+			return Set.copyOf(map(action));
+		}
 
-			for(Runnable r : bindings.get(action)) {
-				handlers.remove(r);
+		/**
+		 * Retrieves the action bound to the given event.
+		 * @param event Event
+		 * @return Bound action
+		 */
+		public Optional<Action<?>> action(Event event) {
+			return Optional.ofNullable(find(event));
+		}
+
+		/**
+		 * Finds the binding for a given event.
+		 */
+		private Action<?> find(Event event) {
+			for(final var entry : bindings.entrySet()) {
+				if(entry.getValue().contains(event)) {
+					return entry.getKey();
+				}
 			}
+			return null;
+		}
 
-			bindings.remove(action);
+		/**
+		 * Binds an event.
+		 * @param <E> Event type
+		 * @param event		Event
+		 * @param action	Action
+		 * @throws IllegalStateException if the action has not been registered or the event is already bound to another action
+		 */
+		public <E extends Event> void bind(E event, Action<E> action) {
+			if(find(event) != null) throw new IllegalStateException("Event is already bound: " + event);
+			final Set<Event> events = map(action);
+			events.add(event);
+			handlers.put(event, () -> action.execute(event));
+		}
+
+		/**
+		 * Removes a binding.
+		 * @param event Bound event
+		 * @throws IllegalStateException if the event is not bound
+		 */
+		public void remove(Event event) {
+			final Action<?> action = find(event);
+			if(action == null) throw new IllegalStateException("Unknown binding: " + event);
+			bindings.get(action).remove(event);
+			handlers.remove(event);
+		}
+
+		/**
+		 * Removes all bindings for the given action.
+		 * @param action Action to un-bind
+		 * @throws IllegalStateException if the action has not been registered
+		 */
+		public void remove(Action<?> action) {
+			final Set<Event> events = map(action);
+			for(Event e : events) {
+				handlers.remove(e);
+			}
+			events.clear();
 		}
 
 		@Override
@@ -50,7 +135,7 @@ public interface Action<E extends Event> {
 
 			// TODO - match buttons
 
-			// Delegate to action
+			// Delegate
 			handler.run();
 		}
 	}
