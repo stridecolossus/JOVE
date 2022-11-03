e64f10dba05b4609a489a2d5dfa1277aa12f0d10
stridecolossus <chris.sarge@gmail.com>
2022-11-03 11:09:32 +0000

    Capsule first cut

---
+++ src/main/java/org/sarge/jove/geometry/Capsule.java
@@ -0,0 +1,97 @@
+package org.sarge.jove.geometry;
+
+import static org.sarge.lib.util.Check.notNull;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.sarge.jove.geometry.Ray.Intersection;
+
+/**
+ * A <i>capsule</i> is a <i>swept sphere</i> volume specified as a given radius about a line segment.
+ * @author Sarge
+ */
+public class Capsule implements Volume {
+	private final Point top, bottom;
+	private final float radius;
+	// TODO - top/bottom should be the AB points?
+
+	/**
+	 * Constructor.
+	 * @param top			Top point
+	 * @param bottom		Bottom point
+	 * @param radius		Radius
+	 */
+	public Capsule(Point top, Point bottom, float radius) {
+		this.top = notNull(top);
+		this.bottom = notNull(bottom);
+		this.radius = radius;
+	}
+
+	/**
+	 * @return Radius
+	 */
+	public float radius() {
+		return radius;
+	}
+
+	@Override
+	public Bounds bounds() {
+		// TODO - this is almost certainly wrong
+		return new Bounds(top, bottom);
+	}
+
+	@Override
+	public boolean contains(Point p) {
+		// TODO
+
+		final Vector segment = Vector.between(bottom, top);
+		final Vector normal = segment.normalize();
+
+		final Point a = bottom.add(normal);
+		final Point b = top.subtract(normal);
+		final Vector ab = Vector.between(a, b);
+
+		final float t = p.subtract(a).dot(ab) / ab.dot(ab);
+		final Point n = a.add(ab.multiply(saturate(t)));
+
+		return n.distance(p) < radius * radius;
+	}
+
+	private static float saturate(float t) {
+		return Math.min(1, Math.max(0, t));
+	}
+
+	@Override
+	public boolean intersects(Volume vol) {
+		if(vol instanceof SphereVolume sphere) {
+			return false; // TOOD
+		}
+		else
+		if(vol instanceof BoundingBox box) {
+			return false; // TOOD
+		}
+		else {
+			return Volume.super.intersects(this);
+		}
+	}
+
+	@Override
+	public boolean intersects(Plane plane) {
+		// TODO
+		return false;
+	}
+
+	@Override
+	public Intersection intersection(Ray ray) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this)
+				.append("start", top)
+				.append("end", bottom)
+				.append("r", radius)
+				.build();
+	}
+}
---
+++ src/test/java/org/sarge/jove/geometry/CapsuleTest.java
@@ -0,0 +1,64 @@
+package org.sarge.jove.geometry;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
+
+class CapsuleTest {
+	private Capsule capsule;
+	private Point top, bottom;
+
+	@BeforeEach
+	void before() {
+		top = new Point(0, 4, 0);
+		bottom = new Point(0, 1, 0);
+		capsule = new Capsule(top, bottom, 2);
+	}
+
+	@Test
+	void constructor() {
+		assertEquals(2, capsule.radius());
+	}
+
+	@DisplayName("A capsule TODO")
+	@Test
+	void bounds() {
+		final Bounds expected = new Bounds(top, bottom);
+		assertEquals(expected, capsule.bounds());
+	}
+
+	@DisplayName("A capsule...")
+	@Nested
+	class ContainTests {
+		@DisplayName("contains the top and bottom points")
+		@Test
+		void ends() {
+			assertEquals(true, capsule.contains(top));
+			assertEquals(true, capsule.contains(bottom));
+		}
+
+		@DisplayName("contains points that lie on the line segment")
+		@Test
+		void segment() {
+			assertEquals(true, capsule.contains(new Point(0, 3, 0)));
+		}
+
+		@DisplayName("does not contain points above the top point")
+		@Test
+		void above() {
+			assertEquals(false, capsule.contains(new Point(0, 5, 0)));
+		}
+
+		@DisplayName("does not contain points below the bottom point")
+		@Test
+		void below() {
+			assertEquals(false, capsule.contains(new Point(0, 0, 0)));
+		}
+
+		@DisplayName("does not contain points outside the capsule radius")
+		@Test
+		void outside() {
+			assertEquals(false, capsule.contains(new Point(0, 2, 3)));
+		}
+	}
+}
